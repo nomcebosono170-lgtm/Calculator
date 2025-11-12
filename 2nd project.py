069e5430a71d1dfd5ea9d0747e30c56ea66feafc
unknown <nomcebosono170-lgtm>
2025-11-12 11:52:05 +0200

    Commit

---
+++ 2nd project.py
@@ -0,0 +1,5 @@
+name = input ("Enter your name: ")
+surname = input ("Enter yout surname: ")
+age = int (input ("Enter your age: "))
+
+print (f"Hello {name}, Have a beautiful day!")
---
+++ My first python.py
@@ -0,0 +1,4 @@
+name = input=("Enter yor name: ")
+age = int(input("Enter your age: "))
+
+print (f"Hello {name}, you are {age} years old!")
